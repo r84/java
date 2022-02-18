84149628f2922a263f735be7f468b9b0f42c5af4
Raphael de matos stanula <39820693+r84@users.noreply.github.com>
2022-02-18 17:48:24 -0300

    Add files via upload
    somando dois valores, onde também comento como declarar variáveis em java.

---
+++ somandoValores.java
@@ -0,0 +1,9 @@
+public class somandoValores{
+    public static void main(String[]args){
+        String titulo = "somando dois valores" ;
+        int n1 = 3;   //devemos sempre dizer o tipo de dado da variável antes de declarar seu nome.
+        int n2 = 4;
+        System.out.println(titulo);
+        System.out.println(n1 + n2); //pedimos para imprimir na tela a soma das duas variáveis.
+}
+}
