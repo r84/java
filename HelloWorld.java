16240e19facd29ece3080fdf9fb97bb9b88e3f8e
Raphael de matos stanula <39820693+r84@users.noreply.github.com>
2022-02-18 00:05:16 -0300

    Add files via upload
    não podia faltar né

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {  //classe onde colocaremos nosso método
+    public static void main(String[] args) { //método onde colocaremos nossa instrução
+        System.out.println("Hello, Wold!");  //comando para imprimirmos a string na tela
+    }
+}
